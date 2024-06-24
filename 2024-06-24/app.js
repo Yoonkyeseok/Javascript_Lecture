40b2fe6b3a6a1bf1fa9cd354347612b056f6dfc0
Yoonkyuseok <yung04240528@gmail.com>
2024-06-24 15:27:54 +0900

    스페이스 인베이더

--- 2024-06-24/app.js
+++ 2024-06-24/app.js
@@ -1,24 +1,26 @@
 const stageSize = 15;
-
 // 스테이지 생성
 for(let i=0; i < stageSize*stageSize; i++) {
     const box = document.createElement("div");
     document.querySelector(".grid").appendChild(box);
 }
-
 const grid = document.querySelector(".grid");
 const stage = document.querySelectorAll(".grid div");
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
 const display = document.querySelector("#display");
-//생략
-let playerLoc = stageSize*(stageSize-2)+7 // 202;
+
+let playerLoc = stageSize*(stageSize-2)+7
+let invaderLoc = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+]
+let gameInterval;
 
 function makePlayer() {
     stage[playerLoc].classList.add("player");
 }
-makePlayer()
-
 function movePlayer(e) {
     stage[playerLoc].classList.remove("player");
     switch (e.keyCode) {
@@ -31,21 +33,57 @@ function movePlayer(e) {
     }
     stage[playerLoc].classList.add("player");
 }
-document.addEventListener("keyup", movePlayer)
-
-let invaderLoc = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
-]
-let gameInterval;
-//생략
 function makeInvader() {
     invaderLoc.forEach(function(invader) {
         stage[invader].classList.add("invader");
     })
+    for(let i=0; i < invaderLoc.length; i++) {
+        invaderLoc[i]++;
+        stage[invaderLoc[i]].classList.add("invader");
+    }
+}
+function moveInvader() {
+    invaderLoc.forEach(function(invader) {
+        stage[invader].classList.remove("invader");
+    })
+    for(let i=0; i < invaderLoc.length; i++) {
+        invaderLoc[i]++;
+        stage[invaderLoc[i]].classList.add("invader");
+    }
 }
 makeInvader();
-function moveInvader() {  }
+
+gameInterval = setInterval(moveInvader, 1000);
 
 interval = setInterval(moveInvader, 1000);
+
+function gameStart() {
+    makePlayer();
+    makeInvader();
+    gameInterval = setInterval(moveInvader, 1000);
+    document.addEventListener("keyup", movePlayer);
+}
+
+function gameStop() {
+    clearInterval(gameInterval);
+    document.removeEventListener("keyup", movePlayer);
+}
+
+function gameRun() {  }
+
+function displayStart() {
+    //생략
+    displayStatus();
+    gameInterval = setInterval(gameRun, 1000); // 수정
+    document.addEventListener("keyup", movePlayer);
+    gameRun();                             // 추가
+}
+function gameRun() {
+    moveInvader();
+}
+function displayStatus() {
+    display.innerText = invaderLoc.length + "/" + invaderLoc.length
+}
+
+startBtn.addEventListener("click", gameStart);
+stopBts.addEventListener("click", gameStop);
